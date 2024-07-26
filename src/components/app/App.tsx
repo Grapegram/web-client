e0198c2b400217262755650b3faea470ca9b74b0
kyrylo <kirillulianow@gmail.com>
2024-07-27 00:37:36 +0300

    initial

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,18 +4,7 @@ function App() {
    
     return (
         <>
-            <div style={{ height: '100vh' }}>
-                <h1
-                    style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                    }}
-                >
-                    Hellow to my basic <i>ts + react</i> template
-                </h1>
-            </div>
+            
         </>
     );
 }
