e23dc31ca90db7b10ce0776b7b856d719772dc93
kyrylo <kirillulianow@gmail.com>
2024-08-21 16:27:10 +0300

    chore: added brand color to variable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,7 @@ module.exports = {
 					DEFAULT: "hsl(var(--card))",
 					foreground: "hsl(var(--card-foreground))",
 				},
+				brand: "#9C36CF",
 			},
 			borderRadius: {
 				lg: "var(--radius)",
