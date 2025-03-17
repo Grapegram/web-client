9e8f237b450d0fbb8d26b657243784761c278e95
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-03-17 12:13:41 +0200

    added pinia state manager

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import './styles/main.css'
+import './app/styles/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 
+import { createPinia } from 'pinia'
+import App from './app/App.vue'
+import router from './app/router'
+
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
