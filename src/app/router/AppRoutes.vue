de16eea293b305cb909302416ffed4760ed9e3f0
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-03-23 23:07:36 +0200

    refactor: moved route types to shared according FSD ruleset

--- src/app/router/AppRoutes.vue
+++ src/app/router/AppRoutes.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import { ROUTES } from '@/shared/lib/routes';
 import { RouterView } from 'vue-router';
 import { useRouter } from 'vue-router';
-import { ROUTES } from '.';
 
 const router = useRouter();
 const isAuth = true; // TODO: implement auth
