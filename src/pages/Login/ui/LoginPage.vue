f23a13153a985336121e9d43ef149e301f58247d
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-03-23 23:11:20 +0200

    feat: implemented login page layout

--- src/pages/Login/ui/LoginPage.vue
+++ src/pages/Login/ui/LoginPage.vue
@@ -1,5 +1,126 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { logo } from '@/shared/assets';
+import { GoogleIcon } from '@/shared/icons';
+import { ROUTES } from '@/shared/lib/routes';
+
+import { Avatar, AvatarImage } from '@/shared/ui/avatar';
+import { Button } from '@/shared/ui/button';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/shared/ui/form';
+import { Input } from '@/shared/ui/input';
+import { Separator } from '@/shared/ui/separator';
+import { toTypedSchema } from '@vee-validate/zod';
+import { useForm } from 'vee-validate';
+import * as z from 'zod';
+import { vAutoAnimate } from '@formkit/auto-animate/vue';
+
+const formSchema = toTypedSchema(
+  z.object({
+    email: z
+      .string()
+      .min(1, { message: 'This field is required.' })
+      .email({ message: 'Incorrect email syntax.' }),
+    password: z
+      .string()
+      .min(1, { message: 'This field is required.' })
+      .min(6, { message: 'Password must be at least 6 characters.' })
+      .regex(/[A-Z]/, {
+        message: 'Password must contain at least one uppercase letter.'
+      })
+      .regex(/[a-z]/, {
+        message: 'Password must contain at least one lowercase letter.'
+      })
+      .regex(/[0-9]/, {
+        message: 'Password must contain at least one number.'
+      })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: 'Password must contain at least one special character.'
+      })
+  })
+);
+
+const { isFieldDirty, handleSubmit } = useForm({
+  validationSchema: formSchema
+});
+
+const onSubmit = handleSubmit(values => {
+  console.log(values);
+});
+</script>
 
 <template>
-  <h1>This is a Login page</h1>
+  <section
+    class="flex w-full max-w-xs flex-col items-center justify-center gap-y-11"
+  >
+    <Avatar class="h-[120px] w-[120px] bg-transparent">
+      <AvatarImage class="object-fill" :src="logo" />
+    </Avatar>
+
+    <div class="flex flex-col items-center justify-center gap-y-1.5">
+      <h1 class="text-2xl font-bold">Log in to Grapegram</h1>
+      <p class="text-center font-thin opacity-50">
+        Please place your credentials in the appropriate fields.
+      </p>
+    </div>
+
+    <form @submit="onSubmit" class="flex w-full flex-col gap-6">
+      <FormField
+        name="email"
+        v-slot="{ componentField }"
+        :validate-on-blur="!isFieldDirty"
+      >
+        <FormItem v-auto-animate>
+          <FormLabel>Email</FormLabel>
+          <FormControl>
+            <Input
+              type="email"
+              placeholder="Your email... promise we won't spam."
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField
+        name="password"
+        v-slot="{ componentField }"
+        :validate-on-blur="!isFieldDirty"
+      >
+        <FormItem v-auto-animate>
+          <FormLabel>Password</FormLabel>
+          <FormControl>
+            <Input
+              type="password"
+              placeholder="Your password... make it strong"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <Button variant="secondary" type="submit">Log in</Button>
+      <Separator label="Or continue with" />
+      <Button variant="outline" class="w-full">
+        <GoogleIcon />
+        Login with Google
+      </Button>
+
+      <div class="text-center text-sm">
+        Don't have an account?
+        <RouterLink
+          :to="ROUTES.SIGNUP"
+          class="text-accent underline underline-offset-4"
+        >
+          Sign up
+        </RouterLink>
+      </div>
+    </form>
+  </section>
 </template>
