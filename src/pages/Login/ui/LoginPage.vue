c37a3fdea96d4ba672aac2fcf2896534df2c8eb9
Blackgolyb <andrejomelnickij@gmail.com>
2025-05-30 18:50:49 +0300

    feat: apply new stand along layout for singup and login pages

--- src/pages/Login/ui/LoginPage.vue
+++ src/pages/Login/ui/LoginPage.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { FormLayout } from '@/widgets/FormLayout';
 import { LoginForm } from '@/widgets/Auth';
+import { StandAlongPageLayout } from '@/widgets/StandAlongPageLayout';
 </script>
 
 <template>
-  <FormLayout
-    title="Welcome back"
-    subtitle="Please place your credentials in the appropriate fields."
-  >
-    <LoginForm />
-  </FormLayout>
+  <StandAlongPageLayout>
+    <FormLayout
+      class="relative top-6/12 left-6/12 -translate-x-2/4 translate-y-[-63%]"
+      title="Welcome back"
+      subtitle="Please place your credentials in the appropriate fields."
+    >
+      <LoginForm />
+    </FormLayout>
+  </StandAlongPageLayout>
 </template>
