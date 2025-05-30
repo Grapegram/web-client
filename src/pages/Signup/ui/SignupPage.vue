abcedb0860d3c1b05db38cf93eec629617bed2c3
Blackgolyb <andrejomelnickij@gmail.com>
2025-05-30 18:50:49 +0300

    feat: apply new stand along layout for singup and login pages

--- src/pages/Signup/ui/SignupPage.vue
+++ src/pages/Signup/ui/SignupPage.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { FormLayout } from '@/widgets/FormLayout';
 import { SignupForm } from '@/widgets/Auth';
+import { StandAlongPageLayout } from '@/widgets/StandAlongPageLayout';
 </script>
 
 <template>
-  <FormLayout
-    title="Join Grapegram"
-    subtitle="Please place your credentials in the appropriate fields."
-  >
-    <SignupForm />
-  </FormLayout>
+  <StandAlongPageLayout>
+    <FormLayout
+      class="relative top-6/12 left-6/12 -translate-x-2/4 translate-y-[-63%]"
+      title="Join Grapegram"
+      subtitle="Please place your credentials in the appropriate fields."
+    >
+      <SignupForm />
+    </FormLayout>
+  </StandAlongPageLayout>
 </template>
