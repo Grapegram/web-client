40296a369014a043082b5d78c74e8733eae66729
Blackgolyb <andrejomelnickij@gmail.com>
2025-05-31 03:17:56 +0300

    refactor: replac const mock data in ChatAvatar with stores

--- src/pages/Home/ui/ChatAvatar.vue
+++ src/pages/Home/ui/ChatAvatar.vue
@@ -1,55 +1,28 @@
 <script lang="ts">
 export type AvaratProps = Pick<Props, 'size'> & {
   class?: HTMLAttributes['class'];
-  chatId: string;
-};
-
-// TODO: move this type to user module
-type Chat = {
-  id: string;
-  name: string;
+  chatId: ChatId;
 };
 </script>
 
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue';
 import Avatar, { type Props } from './AvatarWithColorFallback.vue';
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
+import { useChatStore, type ChatId } from '../model/chat';
 
 const props = defineProps<AvaratProps>();
 
-// TODO: replace with gettig from buisness logic layer
-const chats = ref<Record<string, Chat>>({
-  '0': {
-    id: '0',
-    name: 'Grapegram Team'
-  },
-  '3': {
-    id: '3',
-    name: 'Team 1'
-  },
-  '1': {
-    id: '1',
-    name: 'Kirill'
-  },
-  '23': {
-    id: '23',
-    name: 'Andrew'
-  },
-  '47': {
-    id: '47',
-    name: 'Test 1'
-  }
-});
-
-// INFO: maybe move to buisness logic too
-const userName = computed(() => chats.value[props.chatId]?.name ?? 'Unknown');
+const chatStore = useChatStore();
+const chat = computed(() => chatStore.getChatById(props.chatId));
+const unknownName = 'Unknown';
 </script>
 
 <template>
   <Avatar
-    :alt="userName"
-    :name="userName"
+    :alt="chat?.name ?? unknownName"
+    :src="chat?.avatarURL ?? ''"
+    :name="chat?.name ?? unknownName"
     :class="props.class"
     :size="props.size"
   />
