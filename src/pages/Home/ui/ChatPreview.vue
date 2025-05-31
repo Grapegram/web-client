2f52bc3d6ea7c19283995f8caf9c02716f2b9cee
Blackgolyb <andrejomelnickij@gmail.com>
2025-05-31 03:17:07 +0300

    refactor: replace const mock data in sidebar with stores

--- src/pages/Home/ui/ChatPreview.vue
+++ src/pages/Home/ui/ChatPreview.vue
@@ -1,30 +1,8 @@
 <script lang="ts">
-type UserId = string;
-export type ChatId = string;
-
-type Message = {
-  author: UserId;
-  data: {
-    type: 'text';
-    text: string;
-  };
-  createTime: DateTime;
-  editTime: DateTime;
-};
-
-export type ChatData = {
-  id: ChatId;
-  type: 'group' | 'direct';
-  name: string;
-  avatar: string;
-  unreaded: number;
-  partisipents: string[];
-  messages: Message[];
-};
-
 type Variants = 'compact' | 'expanded';
-type Props = ChatData & {
+type Props = {
   class?: string;
+  chatId: ChatId;
   variant?: Variants;
   isActive: boolean;
   isPinned?: boolean;
@@ -40,12 +18,30 @@ import { cva } from 'class-variance-authority';
 import { Badge } from '@/shared/ui/badge';
 import { Pin } from 'lucide-vue-next';
 import { toRefs } from '@vueuse/core';
+import { useChatStore, type ChatId } from '../model/chat';
+import { useMessagesStore, type Message } from '../model/messages';
 
 const props = defineProps<Props>();
-const { id, type, name, avatar, unreaded, messages, isActive, isPinned } =
-  toRefs(props);
+const { chatId, isActive, isPinned } = toRefs(props);
 const variant = computed<Variants>(() => props.variant ?? 'expanded');
+const emit = defineEmits(['click']);
+
+const chatStore = useChatStore();
+const messagesStore = useMessagesStore();
+const chat = computed(() => chatStore.getChatById(chatId.value));
+const messages = computed(() => messagesStore.getChatMessages(chatId.value));
 const lastMessage = computed(() => messages.value.at(-1));
+// TODO: replace it with find the last message read by the user
+const unreaded = computed(() => 0);
+
+function getMessagePreview(message: Message): string {
+  if (message.data.type === 'text') {
+    return message.data.text;
+  } else if (message.data.type === 'img') {
+    return 'photo';
+  }
+  return 'unknown';
+}
 
 function formatDateTime(date: DateTime): string {
   const now = DateTime.now();
@@ -84,9 +80,10 @@ const chatVariants = cva('', {
         }
       )
     "
+    @click="emit('click')"
   >
     <div class="relative h-auto w-auto">
-      <ChatAvatar class="" :src="avatar" :chat-id="id" />
+      <ChatAvatar class="" :chat-id="chatId" />
       <Badge
         class="absolute right-0 bottom-0 rounded-full px-2"
         v-if="variant === 'compact' && unreaded > 0"
@@ -101,10 +98,10 @@ const chatVariants = cva('', {
       class="flex h-full grow flex-col justify-between self-start overflow-hidden py-1"
     >
       <h3 class="truncate">
-        <strong>{{ name }}</strong>
+        <strong>{{ chat?.name }}</strong>
       </h3>
       <p class="truncate text-gray-300">
-        {{ lastMessage?.data.text }}
+        {{ lastMessage ? getMessagePreview(lastMessage) : '' }}
       </p>
     </div>
 
--- src/pages/Home/ui/Sidebar.vue
+++ src/pages/Home/ui/Sidebar.vue
@@ -1,101 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import ChatPreview, { type ChatData, type ChatId } from './ChatPreview.vue';
+import ChatPreview from './ChatPreview.vue';
 import { ScrollArea } from '@/shared/ui/scroll-area';
 import { inject } from 'vue';
-import { DateTime } from 'luxon';
 import { computed } from 'vue';
-
-const activeChatId = ref('1');
-const pinnedChatsIds = ref(['47', '0']);
-const rawChats = ref<ChatData[]>([
-  {
-    id: '0',
-    type: 'group',
-    name: 'Grapegram Team',
-    avatar: 'url to avatar',
-    unreaded: 101,
-    partisipents: ['0', '1', '2'],
-    messages: []
-  },
-  {
-    id: '3',
-    type: 'group',
-    name: 'Team 1',
-    avatar: 'url to avatar',
-    partisipents: ['0', '1', '2'],
-    unreaded: 1,
-    messages: []
-  },
-  {
-    id: '1',
-    type: 'direct',
-    name: 'Kirill',
-    avatar: 'url to avatar',
-    unreaded: 0,
-    partisipents: ['2'],
-    messages: [
-      {
-        author: '0',
-        data: {
-          type: 'text',
-          text: 'message 1'
-        },
-        editTime: DateTime.fromISO('2023-04-15T14:30:00'),
-        createTime: DateTime.fromISO('2023-04-15T14:30:00')
-      },
-      {
-        author: '2',
-        data: {
-          type: 'text',
-          text: 'message 2'
-        },
-        editTime: DateTime.fromISO('2019-11-27T08:15:45'),
-        createTime: DateTime.fromISO('2019-11-27T08:15:45')
-      }
-    ]
-  },
-  {
-    id: '23',
-    type: 'direct',
-    name: 'Andrew',
-    avatar: 'url to avatar',
-    partisipents: ['1'],
-    unreaded: 23,
-    messages: [
-      {
-        author: '0',
-        data: {
-          type: 'text',
-          text: 'Hi dude!'
-        },
-        editTime: DateTime.fromISO('2025-03-20T22:05:30'),
-        createTime: DateTime.fromISO('2025-03-20T22:05:30')
-      }
-    ]
-  },
-  {
-    id: '47',
-    type: 'direct',
-    name: 'Test 1',
-    avatar: 'url to avatar',
-    partisipents: ['3'],
-    unreaded: 2,
-    messages: [
-      {
-        author: '3',
-        data: {
-          type: 'text',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet, ante in cursus suscipit, enim est rhoncus nisl, vel venenatis nulla felis condimentum felis.'
-        },
-        editTime: DateTime.fromISO('2025-03-23T22:05:30'),
-        createTime: DateTime.fromISO('2025-03-23T22:05:30')
-      }
-    ]
-  }
-]);
+import { useChatStore } from '../model/chat';
+import { useMessagesStore } from '../model/messages';
+import type { Chat, ChatData, ChatId } from '../model/chat';
 
 const sidebarMode = inject<'compact' | 'expanded'>('sidebarMode');
+
+const chatStore = useChatStore();
+const messagesStore = useMessagesStore();
 
 function sortByMessageTime(chats: ChatData[]): ChatData[] {
   const getLastMessageTimeSecs = (chat: ChatData): number =>
@@ -104,21 +19,20 @@ function sortByMessageTime(chats: ChatData[]): ChatData[] {
     getLastMessageTimeSecs(a) < getLastMessageTimeSecs(b) ? 1 : -1
   );
 }
-function getChatById(chats: ChatData[], chatId: ChatId): ChatData | undefined {
-  return chats.find(c => c.id === chatId);
+
+function attachMessagesToChat(chat: Chat) {
+  return { ...chat, messages: messagesStore.getChatMessages(chat.id) };
 }
 
-const pinnedChats = computed<ChatData[]>(() => {
-  return pinnedChatsIds.value.flatMap(id => {
-    const chat = getChatById(rawChats.value, id);
-    return chat ? [chat] : [];
-  });
-});
-const unpinnedChats = computed<ChatData[]>(() => {
-  return sortByMessageTime(rawChats.value).filter(
-    c => !pinnedChatsIds.value.includes(c.id)
-  );
-});
+const activeChatId = computed(() => chatStore.currentChatId);
+const unpinnedChats = computed(() =>
+  sortByMessageTime(chatStore.unpinnedChats.map(attachMessagesToChat))
+);
+const pinnedChats = chatStore.pinnedChats;
+
+function selectThisChat(chatId: ChatId) {
+  chatStore.setCurrentChat(chatId);
+}
 </script>
 
 <template>
@@ -127,17 +41,19 @@ const unpinnedChats = computed<ChatData[]>(() => {
       <ChatPreview
         v-for="chat in pinnedChats"
         :key="chat.id"
-        v-bind="chat"
+        :chat-id="chat.id"
         :variant="sidebarMode"
         :is-active="activeChatId === chat.id"
+        @click="selectThisChat(chat.id)"
         is-pinned
       />
       <ChatPreview
         v-for="chat in unpinnedChats"
         :key="chat.id"
-        v-bind="chat"
+        :chat-id="chat.id"
         :variant="sidebarMode"
         :is-active="activeChatId === chat.id"
+        @click="selectThisChat(chat.id)"
       />
     </ScrollArea>
   </div>
