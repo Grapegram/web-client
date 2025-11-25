f994b316bfa126eb943e38e6db5674d6a38c7435
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-11-25 01:05:44 +0100

    feat: added global toster

--- src/app/router/AppRoutes.vue
+++ src/app/router/AppRoutes.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import { ROUTES } from '@/shared/lib/routes';
 import { RouterView } from 'vue-router';
 import { useRouter } from 'vue-router';
+
+import 'vue-sonner/style.css';
+
+import { ROUTES } from '@/shared/lib/routes';
+import { Toaster } from '@/shared/ui/sonner';
 
 const router = useRouter();
 const isAuth = true; // TODO: implement auth
@@ -17,4 +21,5 @@ router.beforeEach((to, _, next) => {
 
 <template>
   <RouterView />
+  <Toaster position="bottom-left" />
 </template>
