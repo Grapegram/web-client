998a93e7a10af1f450ca728e225369072b34c8d5
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-11-26 12:00:38 +0100

    feat: added stepper component

---
+++ src/shared/ui/stepper/StepperTitle.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue';
+
+import { reactiveOmit } from '@vueuse/core';
+
+import type { StepperTitleProps } from 'reka-ui';
+import { StepperTitle, useForwardProps } from 'reka-ui';
+
+import { cn } from '@/shared/lib/utils';
+
+const props = defineProps<
+  StepperTitleProps & { class?: HTMLAttributes['class'] }
+>();
+
+const delegatedProps = reactiveOmit(props, 'class');
+
+const forwarded = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <StepperTitle
+    v-bind="forwarded"
+    as="div"
+    :class="cn('text-sm font-medium whitespace-nowrap transition', props.class)"
+  >
+    <slot />
+  </StepperTitle>
+</template>
