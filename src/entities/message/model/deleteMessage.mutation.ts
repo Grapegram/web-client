1141fd0c9da9c9a318781ce7508b05734d2260fd
Blackgolyb <andrejomelnickij@gmail.com>
2025-12-04 05:18:25 +0200

    fix: remvoe optimistic sets

--- src/entities/message/model/deleteMessage.mutation.ts
+++ src/entities/message/model/deleteMessage.mutation.ts
@@ -2,19 +2,17 @@ import { useMutation } from '@pinia/colada';
 
 import { MessageApi } from '../api/message.api';
 import type { DeleteMessageRequest } from '../api/message.api.types';
-import { useMessageStore } from './message.store';
 
 export const useDeleteMessageMutation = () =>
   useMutation({
     mutation: (variables: DeleteMessageRequest) => MessageApi.remove(variables),
     onSuccess: (_, variables) => {
-      const messageStore = useMessageStore();
-      // Find the message to get chat_id
-      const allMessages = messageStore.allMessages;
-      const message = allMessages.find(m => m.id === variables.message_id);
-
-      if (message) {
-        messageStore.removeMessage(message.chat_id, variables.message_id);
-      }
+      // const messageStore = useMessageStore();
+      // // Find the message to get chat_id
+      // const allMessages = messageStore.allMessages;
+      // const message = allMessages.find(m => m.id === variables.message_id);
+      // if (message) {
+      //   messageStore.removeMessage(message.chat_id, variables.message_id);
+      // }
     }
   });
