6e0ada72859c344f59743d0c59e6d9f699110b92
Blackgolyb <andrejomelnickij@gmail.com>
2025-12-04 02:45:31 +0200

    feat: improve event emmiter

--- src/shared/lib/event-emitter.ts
+++ src/shared/lib/event-emitter.ts
@@ -4,17 +4,19 @@ export class EventEmitter {
   private events: Record<string, EventHandler[]> = {};
 
   on(event: string, handler: EventHandler) {
-    console.log(`Subscribed to event: ${event}`);
     (this.events[event] ||= []).push(handler);
   }
 
   off(event: string, handler: EventHandler) {
-    console.log(`Unsubscribed from event: ${event}`);
     this.events[event] = (this.events[event] || []).filter(h => h !== handler);
   }
 
   emit(event: string, payload?: unknown) {
-    console.log(`Emitting event: ${event}`);
     (this.events[event] || []).forEach(handler => handler(payload));
+  }
+
+  proxyTo(other: EventEmitter, events: string[]) {
+    for (const event of events)
+      this.on(event, payload => other.emit(event, payload));
   }
 }
