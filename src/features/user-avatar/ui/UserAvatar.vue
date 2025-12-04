9fdbd8b9af243d74fa9c80e7eaae191406432a7a
Blackgolyb <andrejomelnickij@gmail.com>
2025-12-04 02:49:13 +0200

    feat: add is online status to user avatar

--- src/features/user-avatar/ui/UserAvatar.vue
+++ src/features/user-avatar/ui/UserAvatar.vue
@@ -2,6 +2,7 @@
 export type AvaratProps = Pick<Props, 'size'> & {
   class?: HTMLAttributes['class'];
   userId: string;
+  showStatus?: boolean;
 };
 </script>
 
@@ -22,14 +23,62 @@ const user = computed(
 );
 const userName = computed(() => user.value?.username ?? 'Unknown');
 const userAvatar = computed(() => user.value?.avatar);
+const isOnline = computed(() => user.value?.isOnline ?? false);
 </script>
 
 <template>
-  <Avatar
-    :src="userAvatar"
-    :alt="userName"
-    :name="userName"
-    :class="props.class"
-    :size="props.size"
-  />
+  <div :class="props.class">
+    <!-- <div
+      v-if="props.showStatus"
+      class="relative"
+      :style="{
+        maskImage: isOnline
+          ? `radial-gradient(circle at calc(100% - 6px) calc(100% - 6px), transparent 0, transparent ${
+              props.size === 'sm'
+                ? '7px'
+                : props.size === 'lg'
+                  ? '10px'
+                  : '8.5px'
+            }, black ${
+              props.size === 'sm'
+                ? '7px'
+                : props.size === 'lg'
+                  ? '10px'
+                  : '8.5px'
+            })`
+          : undefined,
+        WebkitMaskImage: isOnline
+          ? `radial-gradient(circle at calc(100% - 6px) calc(100% - 6px), transparent 0, transparent ${
+              props.size === 'sm'
+                ? '7px'
+                : props.size === 'lg'
+                  ? '10px'
+                  : '8.5px'
+            }, black ${
+              props.size === 'sm'
+                ? '7px'
+                : props.size === 'lg'
+                  ? '10px'
+                  : '8.5px'
+            })`
+          : undefined
+      }"
+    > -->
+    <Avatar
+      :src="userAvatar ?? ''"
+      :alt="userName"
+      :name="userName"
+      :size="props.size"
+    />
+    <!-- </div> -->
+    <span
+      v-if="props.showStatus && isOnline"
+      class="bg-accent absolute right-0 bottom-0 z-50 block rounded-full"
+      :class="{
+        'size-2': props.size === 'sm',
+        'size-3 -translate-1': props.size === 'base' || !props.size,
+        'size-4 -translate-1': props.size === 'lg'
+      }"
+    />
+  </div>
 </template>
